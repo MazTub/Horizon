1deb56c3dca7ef84cf73074e0a5336988a9eef13
MazTub <thomas.mazhar-elstub@paconsulting.com>
2025-05-09 14:21:07 +0100

    Initial Commit

---
+++ Weekend HorizonTests/Weekend_HorizonTests.swift
@@ -0,0 +1,17 @@
+//
+//  Weekend_HorizonTests.swift
+//  Weekend HorizonTests
+//
+//  Created by Thomas Mazhar-Elstub on 09/05/2025.
+//
+
+import Testing
+@testable import Weekend_Horizon
+
+struct Weekend_HorizonTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
